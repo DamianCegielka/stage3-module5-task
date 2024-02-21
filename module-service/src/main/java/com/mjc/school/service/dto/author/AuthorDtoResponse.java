8a0b8360a6adab6044642efd85d8692267344589
Admin <cegielka.damian@gmail.com>
2024-02-21 20:20:01 +0100

    Add dependency and constructors

--- module-service/src/main/java/com/mjc/school/service/dto/author/AuthorDtoResponse.java
+++ module-service/src/main/java/com/mjc/school/service/dto/author/AuthorDtoResponse.java
@@ -1,15 +1,20 @@
 package com.mjc.school.service.dto.author;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthorDtoResponse {
 
     private Long id;
     private String name;
     private LocalDateTime createDate;
     private LocalDateTime lastUpdateTime;
+
 
 }
